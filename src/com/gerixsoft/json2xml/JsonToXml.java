bbadaca988334da5134159667d001f244d123b91
Andriy Gerasika <andriy.gerasika@gmail.com>
2010-06-28 13:11:40 +0000

    redo from dom to sax

--- src/com/gerixsoft/json2xml/JsonToXml.java
+++ src/com/gerixsoft/json2xml/JsonToXml.java
@@ -6,53 +6,53 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import org.w3c.dom.Document;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.AttributesImpl;
 
 public class JsonToXml {
 
 	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException {
-		DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document document = documentBuilder.newDocument();
-		document.appendChild(document.createElement("json"));
-		String json;
+		SAXTransformerFactory handlerFactory = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
+		TransformerHandler handler = handlerFactory.newTransformerHandler(new StreamSource(JsonToXml.class.getResource("main.xsl").toString()));
+		handler.setResult(new StreamResult(new File("files/output.xml")));
+		handler.startDocument();
+		handler.startElement("", "json", "json", new AttributesImpl());
 		{
-			InputStream inputStream = new FileInputStream(new File("files/input.json"));
-			try {
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			char[] text;
+			{
+				InputStream inputStream = new FileInputStream(new File("files/input.json"));
 				try {
-					byte[] buffer = new byte[65535];
-					int bytesRead;
-					while ((bytesRead = inputStream.read(buffer)) > 0) {
-						outputStream.write(buffer, 0, bytesRead);
+					ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+					try {
+						byte[] buffer = new byte[65535];
+						int bytesRead;
+						while ((bytesRead = inputStream.read(buffer)) > 0) {
+							outputStream.write(buffer, 0, bytesRead);
+						}
+						text = outputStream.toString().toCharArray();
+					} finally {
+						outputStream.close();
 					}
-					json = outputStream.toString();
 				} finally {
-					outputStream.close();
+					inputStream.close();
 				}
-			} finally {
-				inputStream.close();
 			}
+			handler.characters(text, 0, text.length);
 		}
-		document.getDocumentElement().appendChild(document.createTextNode(json));
-
-		Transformer transformer = TransformerFactory.newInstance().newTransformer(new StreamSource(JsonToXml.class.getResource("main.xsl").toString()));
-		transformer.transform(new DOMSource(document), new StreamResult(new File("files/output.xml")));
+		handler.endElement("", "json", "json");
+		handler.endDocument();
 
 		SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
 		Schema schema = factory.newSchema(new StreamSource(JsonToXml.class.getResourceAsStream("json.xsd")));
